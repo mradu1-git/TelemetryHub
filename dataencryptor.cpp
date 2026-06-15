1c4bd762330a81c31409dc62d119251e95fc3ec1
Mradul Pal <mradulpal@outlook.com>
2026-06-15 10:39:11 +0530

    DataEncryptor Class Completed

--- dataencryptor.cpp
+++ dataencryptor.cpp
@@ -1,3 +1,21 @@
-#include "dataencryptor.h"
+#include "DataEncryptor.h"
+#include <QDebug>
 
-DataEncryptor::DataEncryptor() {}
+DataEncryptor::DataEncryptor(QObject *parent) : QObject(parent)
+{
+    // Non-visual elements handle data, networks, or encryption algorithms
+}
+
+QString DataEncryptor::encryptData(const QString &rawData)
+{
+    qDebug() << "C++ Encryptor received raw string:" << rawData;
+
+    // A simple Caesar Cipher simulation for demonstration
+    QString encrypted = rawData;
+    for (int i = 0; i < encrypted.length(); ++i) {
+        encrypted[i] = QChar(encrypted[i].unicode() + 1);
+    }
+
+    qDebug() << "C++ Encryptor returning processed string:" << encrypted;
+    return encrypted;
+}
